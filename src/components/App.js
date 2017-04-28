bbe71693721f81713db9fb65d095587ca9b1992a
chibieee <gregneo21@gmail.com>
2017-04-27 22:35:07 -0400

    added retrieving song data from api logic

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,30 @@ import React, { Component } from 'react';
 
 import SearchBar from './searchbar/searchbar';
 import searchSpotify from '../utility/searchspotify';
+import SongItem from './songitem/songitem';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       initialMessage: 'greeting',
-      song: ''
+      song: '',
+      tracks: {}
     };
   }
 
   fetchSongs = () => {
     searchSpotify(this.state.song)
-      .then((tracks) => console.log(tracks));
+      .then(({ tracks }) => this.setState({ tracks }));
   }
 
   render() {
-    const { initialMessage, song } = this.state;
-    console.log(song);
+    const { tracks } = this.state;
+    console.log(tracks);
     return (
       <div>
-        {initialMessage}
         <SearchBar updateText={(songName) => this.setState({ song: songName })} fetchSongs={this.fetchSongs}/>
+        {tracks.items && <SongItem songData={tracks.items[0]}/>}
       </div>
     );
   }
