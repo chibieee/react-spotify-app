45ce77865fb3251b1a4734731bc9ff1def363799
chibieee <gregneo21@gmail.com>
2017-05-01 22:09:16 -0400

    finished styling

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -3,7 +3,7 @@ import './SearchBar.css';
 
 const SearchBar = (props) => (
     <div className="searchbar-root">
-        <input className="searchbar-input" type="text" onChange={ (e) => props.updateText(e.target.value)}/>
+        <input className="searchbar-input" type="text" onChange={ (e) => props.updateText(e.target.value)} placeholder="Search for an artist or song"/>
         <button className="searchbar-button" onClick={props.fetchSongs}>Get Songs</button>
     </div>
 );
